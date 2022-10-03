54320cc43de777c235d4ed1ca3370dfbe4096d61
Shamsur <shamsur314@gmail.com>
2022-10-03 21:10:33 +0300

    added state to the form

--- frontend/src/components/AddYourName.js
+++ frontend/src/components/AddYourName.js
@@ -1,30 +1,41 @@
+import { useState } from "react";
+
 const AddYourName = () => {
+    const [name, setName] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(name);
+    };
+
 	return (
 		<div>
 			<div className='flex items-center justify-center mt-5'>
-				<div class='w-full max-w-xs'>
+				<div className='w-full max-w-xs'>
 					<h1 className='text-center text-lg block text-gray-700 font-bold mb-2'>
 						Add Your Name
 					</h1>
-					<form class='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
-						<div class='mb-4'>
+					<form className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
+						<div className='mb-4'>
 							<label
-								class='block text-gray-700 text-sm font-bold mb-2'
-								for='username'>
+								className='block text-gray-700 text-sm font-bold mb-2'
+								htmlFor='username'>
 								Name
 							</label>
 							<input
-								class='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+								className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
 								id='username'
 								type='text'
 								placeholder='Username'
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
 							/>
 						</div>
 
-						<div class='flex items-center justify-between'>
+						<div className='flex items-center justify-between'>
 							<button
-								class='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
-								type='button'>
+								className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
+								type='button' onClick={handleSubmit}>
 								Add
 							</button>
 						</div>
