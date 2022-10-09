83f3d6dfbff5011209e31b261d774c289e75109b
Shamsur <shamsur314@gmail.com>
2022-10-10 00:56:25 +0300

    loads all names

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import AddYourName from "./components/AddYourName";
 import ListOfNames from "./components/ListOfNames";
 
@@ -11,17 +12,34 @@ const NAMES = [
 	{
 		name: "John Smith",
 	},
-]
+];
+
+const API = "http://localhost:8080";
 
 function App() {
-  const handleName = (name) => {
-    console.log(name);
-  };
+	const handleName = (name) => {
+		console.log(name);
+	};
+
+	const [allNames, setAllNames] = useState([]);
+
+	useEffect(() => {
+		fetch(`${API}/api`)
+			.then((res) => res.json())
+			.then((data) => {
+				//only add the last 5 names
+				setAllNames(data.slice(-5));
+			});
+	}, []);
 
 	return (
 		<div className='App'>
 			<AddYourName handleName={handleName} />
-      		<ListOfNames allNames={NAMES} />
+			{allNames.length > 0 ? (
+				<ListOfNames allNames={allNames} />
+			) : (
+				<ListOfNames allNames={NAMES} />
+			)}
 		</div>
 	);
 }
